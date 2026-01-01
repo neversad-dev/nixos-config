fb0bce4e2652d0b806a450ff52700cc46aa9ea9a
neversad-dev <7419136+neversad-dev@users.noreply.github.com>
2026-01-01 22:22:52 +0200

    video4

--- home/common/default.nix
+++ home/common/default.nix
@@ -35,7 +35,7 @@
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings = {
-      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
     };
   };
--- home/neversad/dell.nix
+++ home/neversad/dell.nix
@@ -3,11 +3,17 @@
     ./home.nix
     ../common
     ../features/cli
+    ../features/desktop
   ];
 
   features = {
     cli = {
       zsh.enable = true;
+      fzf.enable = true;
+      neofetch.enable = true;
+    };
+    desktop = {
+      wayland.enable = true;
     };
   };
 }
