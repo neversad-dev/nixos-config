3d757a6311b4ec7c89a0e7eae7dfd20e290e7cf2
neversad-dev <7419136+neversad-dev@users.noreply.github.com>
2026-01-04 02:06:34 +0200

    refactor secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,18 @@
 let
   dell = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2wKzWxbeENQIIt9McuGvyVmAvwpcaWIcY9NH5hVbL3";
+
+  systems = [
+    dell
+  ];
+
   neversad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGT4wDVwwRefziU8NxEKI1c+7tqMZM7afXsyvN3kJPp neversad@mbair";
+
+  users = [
+    neversad
+  ];
 in {
-  # agenix -e secret1.age
-  "secret1.age".publicKeys = [dell neversad];
-  "neversad-secrets.age".publicKeys = [dell neversad];
+  # create/edit secrets with: agenix -e secret1.age
+  # after editing publicKeys run: agenix --rekey -i [path to private key]
+  "secret1.age".publicKeys = systems ++ users;
+  "neversad-secrets.age".publicKeys = systems ++ users;
 }
